3787eda5f4064778eebdc2c48359d152e5977257
Sameh Mabrouk <mabrouksameh@gmail.com>
2020-01-16 22:36:06 +0100

    Add tests for linkedlist

--- LinkedList/LinkedList.playground/Contents.swift
+++ LinkedList/LinkedList.playground/Contents.swift
@@ -1,4 +1,4 @@
-class LinkedList<T> {
+final class LinkedList<T> {
     
     // LinkedListNode
     class LinkedListNode<T> {
@@ -11,9 +11,12 @@ class LinkedList<T> {
         }
     }
     
+    /// Default initializer
+    init() {}
+    
     typealias Node = LinkedListNode<T>
     
-    private var head: Node?
+    private(set) var head: Node?
     
     var isEmpty: Bool {
         return head == nil
@@ -105,19 +108,19 @@ class LinkedList<T> {
     }
     
     public func remove(_ node: Node) -> T {
-      let prev = node.previous
-      let next = node.next
-
-      if let prev = prev {
-        prev.next = next
-      } else {
-        head = next
-      }
-      next?.previous = prev
-
-      node.previous = nil
-      node.next = nil
-      return node.value
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        next?.previous = prev
+        
+        node.previous = nil
+        node.next = nil
+        return node.value
     }
     
     func removeLast() -> T {
@@ -132,19 +135,19 @@ class LinkedList<T> {
     }
     
     func reverse() {
-      var node = head
-//      tail = node // If you had a tail pointer
-      while let currentNode = node {
-        node = currentNode.next
-        print("[currentNode]: \(String(describing: currentNode.value))")
-        print("[currentNode.next]: \(String(describing: currentNode.next?.value))")
-        print("[currentNode.previous]: \(String(describing: currentNode.previous?.value))")
-        print("Description before swapping: \(description)")
-        swap(&currentNode.next, &currentNode.previous)
-        head = currentNode
-        print("Head: \(String(describing: head?.value))")
-        print("Description: \(description)")
-      }
+        var node = head
+        //      tail = node // If you had a tail pointer
+        while let currentNode = node {
+            node = currentNode.next
+            print("[currentNode]: \(String(describing: currentNode.value))")
+            print("[currentNode.next]: \(String(describing: currentNode.next?.value))")
+            print("[currentNode.previous]: \(String(describing: currentNode.previous?.value))")
+            print("Description before swapping: \(description)")
+            swap(&currentNode.next, &currentNode.previous)
+            head = currentNode
+            print("Head: \(String(describing: head?.value))")
+            print("Description: \(description)")
+        }
     }
     
     // Map
@@ -173,6 +176,25 @@ extension LinkedList {
             node = nd.next
         }
         return result
+    }
+}
+
+
+// MARK: - Extension to enable initialization from an Array
+extension LinkedList {
+    convenience init(array: Array<T>) {
+        self.init()
+        
+        array.forEach { append(value: $0) }
+    }
+}
+
+// MARK: - Extension to enable initialization from an Array Literal
+extension LinkedList: ExpressibleByArrayLiteral {
+    convenience init(arrayLiteral elements: T...) {
+        self.init()
+        
+        elements.forEach { append(value: $0) }
     }
 }
 
@@ -322,3 +344,301 @@ let filteredList = stringlist.filter { s in
     s.count > 5
 }
 filteredList
+
+import XCTest
+// MARK: - Unit testing
+class LinkedListTests: XCTestCase {
+    // MARK: - Test Properties
+    
+    let numbers = [8, 2, 10, 9, 7, 5]
+    
+    fileprivate func buildList() -> LinkedList<Int> {
+        let list = LinkedList<Int>()
+        for number in numbers {
+            list.append(value: number)
+        }
+        return list
+    }
+    
+    func testSwift4() {
+        // last checked with Xcode 9.0b4
+        #if swift(>=4.0)
+        print("Hello, Swift 4!")
+        #endif
+    }
+    
+    func testEmptyList() {
+        let list = LinkedList<Int>()
+        XCTAssertTrue(list.isEmpty)
+        XCTAssertEqual(list.count, 0)
+        XCTAssertNil(list.first)
+        XCTAssertNil(list.last)
+    }
+    
+    func testListWithOneElement() {
+        let list = LinkedList<Int>()
+        list.append(value: 123)
+        
+        XCTAssertFalse(list.isEmpty)
+        XCTAssertEqual(list.count, 1)
+        
+        XCTAssertNotNil(list.first)
+        XCTAssertNil(list.head!.previous)
+        XCTAssertNil(list.head!.next)
+        XCTAssertEqual(list.head!.value, 123)
+        
+        XCTAssertNotNil(list.last)
+        XCTAssertNil(list.last!.previous)
+        XCTAssertNil(list.last!.next)
+        XCTAssertEqual(list.last!.value, 123)
+        
+        XCTAssertTrue(list.head === list.last)
+    }
+    
+    func testListWithTwoElements() {
+        let list = LinkedList<Int>()
+        list.append(value: 123)
+        list.append(value: 456)
+        
+        XCTAssertEqual(list.count, 2)
+        
+        XCTAssertNotNil(list.first)
+        XCTAssertEqual(list.head!.value, 123)
+        
+        XCTAssertNotNil(list.last)
+        XCTAssertEqual(list.last!.value, 456)
+        
+        XCTAssertTrue(list.head !== list.last)
+        
+        XCTAssertNil(list.head!.previous)
+        XCTAssertTrue(list.head!.next === list.last)
+        XCTAssertTrue(list.last!.previous === list.head)
+        XCTAssertNil(list.last!.next)
+    }
+    
+    func testListWithThreeElements() {
+        let list = LinkedList<Int>()
+        list.append(value: 123)
+        list.append(value: 456)
+        list.append(value: 789)
+        
+        XCTAssertEqual(list.count, 3)
+        
+        XCTAssertNotNil(list.first)
+        XCTAssertEqual(list.head!.value, 123)
+        
+        let second = list.head!.next
+        XCTAssertNotNil(second)
+        XCTAssertEqual(second!.value, 456)
+        
+        XCTAssertNotNil(list.last)
+        XCTAssertEqual(list.last!.value, 789)
+        
+        XCTAssertNil(list.head!.previous)
+        XCTAssertTrue(list.head!.next === second)
+        XCTAssertTrue(second!.previous === list.head)
+        XCTAssertTrue(second!.next === list.last)
+        XCTAssertTrue(list.last!.previous === second)
+        XCTAssertNil(list.last!.next)
+    }
+    
+    func testNodeAtIndexInListWithOneElement() {
+        let list = LinkedList<Int>()
+        list.append(value: 123)
+        
+        let node = list.node(atIndex: 0)
+        XCTAssertNotNil(node)
+        XCTAssertEqual(node.value, 123)
+        XCTAssertTrue(node === list.head)
+    }
+    
+    func testNodeAtIndex() {
+        let list = buildList()
+        
+        let nodeCount = list.count
+        XCTAssertEqual(nodeCount, numbers.count)
+        
+        let first = list.node(atIndex: 0)
+        XCTAssertNotNil(first)
+        XCTAssertTrue(first === list.head)
+        XCTAssertEqual(first.value, numbers[0])
+        
+        let last = list.node(atIndex: nodeCount - 1)
+        XCTAssertNotNil(last)
+        XCTAssertTrue(last === list.last)
+        XCTAssertEqual(last.value, numbers[nodeCount - 1])
+        
+        for i in 0..<nodeCount {
+            let node = list.node(atIndex: i)
+            XCTAssertNotNil(node)
+            XCTAssertEqual(node.value, numbers[i])
+        }
+    }
+    
+    func testSubscript() {
+        let list = buildList()
+        for i in 0 ..< list.count {
+            XCTAssertEqual(list[i], numbers[i])
+        }
+    }
+    
+    func testInsertAtIndexInEmptyList() {
+        let list = LinkedList<Int>()
+        list.insert(123, atIndex: 0)
+        
+        XCTAssertFalse(list.isEmpty)
+        XCTAssertEqual(list.count, 1)
+        
+        let node = list.node(atIndex: 0)
+        XCTAssertNotNil(node)
+        XCTAssertEqual(node.value, 123)
+    }
+    
+    func testInsertAtIndex() {
+        let list = buildList()
+        let prev = list.node(atIndex: 2)
+        let next = list.node(atIndex: 3)
+        let nodeCount = list.count
+        
+        list.insert(444, atIndex: 3)
+        
+        let node = list.node(atIndex: 3)
+        XCTAssertNotNil(node)
+        XCTAssertEqual(node.value, 444)
+        XCTAssertEqual(nodeCount + 1, list.count)
+        
+        XCTAssertFalse(prev === node)
+        XCTAssertFalse(next === node)
+        XCTAssertTrue(prev.next === node)
+        XCTAssertTrue(next.previous === node)
+    }
+
+    
+    func testRemoveAtIndexOnListWithOneElement() {
+        let list = LinkedList<Int>()
+        list.append(value: 123)
+        
+        let value = list.removeAt(0)
+        XCTAssertEqual(value, 123)
+        
+        XCTAssertTrue(list.isEmpty)
+        XCTAssertEqual(list.count, 0)
+        XCTAssertNil(list.first)
+        XCTAssertNil(list.last)
+    }
+    
+    func testRemoveAtIndex() {
+        let list = buildList()
+        let prev = list.node(atIndex: 2)
+        let next = list.node(atIndex: 3)
+        let nodeCount = list.count
+        
+        list.insert(444, atIndex: 3)
+        
+        let value = list.removeAt(3)
+        XCTAssertEqual(value, 444)
+        
+        let node = list.node(atIndex: 3)
+        XCTAssertTrue(next === node)
+        XCTAssertTrue(prev.next === node)
+        XCTAssertTrue(node.previous === prev)
+        XCTAssertEqual(nodeCount, list.count)
+    }
+    
+    func testRemoveLastOnListWithOneElement() {
+        let list = LinkedList<Int>()
+        list.append(value: 123)
+        
+        let value = list.removeLast()
+        XCTAssertEqual(value, 123)
+        
+        XCTAssertTrue(list.isEmpty)
+        XCTAssertEqual(list.count, 0)
+        XCTAssertNil(list.first)
+        XCTAssertNil(list.last)
+    }
+    
+    func testRemoveLast() {
+        let list = buildList()
+        let last = list.last
+        let prev = last!.previous
+        let nodeCount = list.count
+        
+        let value = list.removeLast()
+        XCTAssertEqual(value, 5)
+        
+        XCTAssertNil(last!.previous)
+        XCTAssertNil(last!.next)
+        
+        XCTAssertNil(prev!.next)
+        XCTAssertTrue(list.last === prev)
+        XCTAssertEqual(nodeCount - 1, list.count)
+    }
+    
+    func testRemoveAll() {
+        let list = buildList()
+        list.removeAll()
+        XCTAssertTrue(list.isEmpty)
+        XCTAssertEqual(list.count, 0)
+        XCTAssertNil(list.first)
+        XCTAssertNil(list.last)
+    }
+    
+    func testReverseLinkedList() {
+        let list = buildList()
+        let first = list.head
+        let last = list.last
+        let nodeCount = list.count
+        
+        list.reverse()
+        
+        XCTAssertTrue(first === list.last)
+        XCTAssertTrue(last === list.head)
+        XCTAssertEqual(nodeCount, list.count)
+    }
+    
+    func testArrayLiteralInitTypeInfer() {
+        let arrayLiteralInitInfer: LinkedList = [1.0, 2.0, 3.0]
+        
+        XCTAssertEqual(arrayLiteralInitInfer.count, 3)
+        XCTAssertEqual(arrayLiteralInitInfer.head?.value, 1.0)
+        XCTAssertEqual(arrayLiteralInitInfer.last?.value, 3.0)
+        XCTAssertEqual(arrayLiteralInitInfer[1], 2.0)
+        XCTAssertEqual(arrayLiteralInitInfer.removeLast(), 3.0)
+        XCTAssertEqual(arrayLiteralInitInfer.count, 2)
+    }
+    
+    func testArrayLiteralInitExplicit() {
+        let arrayLiteralInitExplicit: LinkedList<Int> = [1, 2, 3]
+
+        XCTAssertEqual(arrayLiteralInitExplicit.count, 3)
+        XCTAssertEqual(arrayLiteralInitExplicit.head?.value, 1)
+        XCTAssertEqual(arrayLiteralInitExplicit.last?.value, 3)
+        XCTAssertEqual(arrayLiteralInitExplicit[1], 2)
+        XCTAssertEqual(arrayLiteralInitExplicit.removeLast(), 3)
+        XCTAssertEqual(arrayLiteralInitExplicit.count, 2)
+    }
+    
+    func testConformanceToCollectionProtocol() {
+        let collection: LinkedList<Int> = [1, 2, 3, 4, 5]
+        let index2 = collection.index(collection.startIndex, offsetBy: 2)
+        let value = collection[index2]
+        
+        XCTAssertTrue(value == 3)
+    }
+}
+
+// To not miss failures, this test observer triggers an assertion failure in case of a test failure.
+class TestObserver: NSObject, XCTestObservation {
+    func testCase(_ testCase: XCTestCase,
+                  didFailWithDescription description: String,
+                  inFile filePath: String?,
+                  atLine lineNumber: Int) {
+        assertionFailure(description, line: UInt(lineNumber))
+    }
+}
+
+let testObserver = TestObserver()
+XCTestObservationCenter.shared.addTestObserver(testObserver)
+// run tests with defaultTestSuite
+LinkedListTests.defaultTestSuite.run()
